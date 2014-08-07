a5bf09d37f8ceafce1bac3aff836bde8507dbf86
Lauri Pesonen <lauri.pesonen@iki.fi>
2014-08-07 18:07:08 +0100

    Upgrade raven-clj and add clojars related fields

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "https://github.com/liwp/corax"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/liwp/corax"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[clj-stacktrace "0.2.8"]
                  [org.clojure/clojure "1.6.0"]
-                 [raven-clj "0.9.0"]])
+                 [raven-clj "1.0.2"]])
