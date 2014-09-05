76b93de3e46829b510dd02138b8606a1b80d02af
Lauri Pesonen <lauri.pesonen@iki.fi>
2014-09-05 15:39:45 +0100

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject corax "0.1.0"
+(defproject corax "0.1.1-SNAPSHOT"
   :description "A layer of sugar on raven-clj"
   :url "https://github.com/liwp/corax"
   :license {:name "Eclipse Public License"
