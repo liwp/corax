5743d538b046de5d01766d542955029854d72661
Lauri Pesonen <lauri.pesonen@iki.fi>
2014-10-31 12:33:04 +0000

    Change the lein project name to listora/corax
    Also, bump up the version number to 0.2.0-SNAPSHOT for the iminent release.
    
    Fixes #7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject corax "0.1.2-SNAPSHOT"
+(defproject listora/corax "0.2.0-SNAPSHOT"
   :description "A layer of sugar on raven-clj"
   :url "https://github.com/liwp/corax"
   :license {:name "Eclipse Public License"
